07c6aed370391151117b170395d87d60a34b3b7f
Yael Zamora <ajedrezkasparov@gmail.com>
2024-11-13 16:54:39 -0600

    Initial Commit

---
+++ SwiftUIDALLE/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIDALLE
+//
+//  Created by Yael Javier Zamora Moreno on 13/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIDALLE/SwiftUIDALLEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDALLEApp.swift
+//  SwiftUIDALLE
+//
+//  Created by Yael Javier Zamora Moreno on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDALLEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
